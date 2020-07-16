b10cec1cb0a7c9c782dc59f58c1e145750645fa3
alihassan <24819103+thisalihassan@users.noreply.github.com>
2020-07-16 17:47:48 +0500

    npm reduce size and favicon

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,27 +1,20 @@
 import * as reduxModule from "redux";
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, createStore, compose } from "redux";
 
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
-import { composeWithDevTools } from "redux-devtools-extension";
-/*
-Fix for Firefox redux dev tools extension
-https://github.com/zalmoxisus/redux-devtools-instrument/pull/19#issuecomment-400637274
- */
+// import { composeWithDevTools } from "redux-devtools-extension";
+
 reduxModule.__DO_NOT_USE__ActionTypes.REPLACE = "@@redux/INIT";
 
-const composeEnhancers =
-  process.env.NODE_ENV !== "production" &&
-  typeof window === "object" &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-      })
-    : composeWithDevTools;
-// const composeEnhancers = composeWithDevTools({
-//   name: 'MyApp', actionsBlacklist: ['REDUX_STORAGE_SAVE']
-// });
-const enhancer = composeEnhancers(applyMiddleware(thunk));
+// const composeEnhancers =
+//   process.env.NODE_ENV !== "production" &&
+//   typeof window === "object" &&
+//   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+//     : composeWithDevTools;
+
+const enhancer = compose(applyMiddleware(thunk));
 
 const store = createStore(rootReducer, enhancer);
 
