c9e8a50d35a45f84f54845674ecdc8434b8914ad
alihassan <24819103+thisalihassan@users.noreply.github.com>
2020-07-16 18:01:28 +0500

    reinstalling devTools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,20 +1,25 @@
 import * as reduxModule from "redux";
-import { applyMiddleware, createStore, compose } from "redux";
+import { applyMiddleware, createStore } from "redux";
 
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
-// import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 reduxModule.__DO_NOT_USE__ActionTypes.REPLACE = "@@redux/INIT";
 
-// const composeEnhancers =
-//   process.env.NODE_ENV !== "production" &&
-//   typeof window === "object" &&
-//   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-//     : composeWithDevTools;
+const composeEnhancers =
+  process.env.NODE_ENV !== "production" &&
+  typeof window === "object" &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : composeWithDevTools;
 
-const enhancer = compose(applyMiddleware(thunk));
+// const composeEnhancers = composeWithDevTools({
+//   name: 'MyApp', actionsBlacklist: ['REDUX_STORAGE_SAVE']
+// });
+const enhancer = composeEnhancers(applyMiddleware(thunk));
 
 const store = createStore(rootReducer, enhancer);
 
